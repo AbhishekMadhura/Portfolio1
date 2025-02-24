a94329710f161343a0c34be96ba3dd39e1460027
abhishek <abhishekmadhura34@gmail.com>
2025-02-24 14:19:05 +0530

    initial

---
+++ src/Components/About.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Skills from './Skills.js';
+import Tilt from 'react-parallax-tilt';
+import Lottie from "lottie-react"
+import Coder from '../LottieFiles/coder.json';
+
+const About = () => {
+  return (
+    <>
+      <div className='AboutPage'>
+        <div className='AboutText'>
+          <h1 className='AboutTextHeading' >Get to <b>know</b> me!</h1>
+          <p>
+            Hi, my name is <b>Abhishek Madhura</b> and I am from Odalarevu, India.
+            I'm a <b>MERN stack web developer</b> and a final year college student pursuing <b>BTech in CSE</b>. <br/><br/>
+            I love to create original projects with beautiful designs, you can check out some of my work in the projects section.<br/><br/>
+            I am <b>open</b> to new collaborations or work where I can contribute and grow. Feel free to connect with me, links are in the footer.<br/>
+            Apart from coding I love <b>Composing Music</b>, you can check out some of my shots here <a href="https://www.instagram.com/official_abhi1_/?hl=en" >Instagram.</a>
+          </p>
+        </div>
+
+        <div>
+        
+          <Tilt>
+              <Lottie 
+              className="illustration" 
+              animationData={Coder} 
+              loop={true} 
+            />
+          </Tilt>
+        </div>
+
+      </div>
+      
+      <h1 className='SkillsHeading'>Professional Skillset</h1>
+      <div className='skills'>
+      <Skills skill='C++' />
+        <Skills skill='React' />
+        <Skills skill='Node' />
+     
+        <Skills skill='MongoDb' />
+        <Skills skill='Git' />
+        <Skills skill='Github' />
+        <Skills skill='Javascript' />
+       
+        <Skills skill='Python' />
+      
+      
+        
+      </div>
+    </>
+  )
+}
+
+export default About
---
+++ src/Components/ProjectBox.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {FaGithub} from "react-icons/fa";
+import {CgFileDocument} from "react-icons/cg";
+
+
+const  ProjectBox = ({projectPhoto, projectName}) => {
+  const desc = {
+    StaffManagementSystemDesc : "The system allows easy access and updating of employee data. It generates timely reports to help monitor employees and support tasks like performance appraisals and promotions",
+    StaffManagementSystemGithub : "https://github.com/AbhishekMadhura/StaffManagementSystem",
+    StaffManagementSystemWebsite : "https://devanshsahni.github.io/tindog/",
+
+    SentimentAnalysisDesc : "Developed a machine learning model using Natural Language Processing (NLP) to classify the sentiment  of customer reviews, enabling the company to identify key areas for improvement and enhance customer satisfaction.",
+    SentimentAnalysisGithub : "https://github.com/DevanshSahni/Rog-Free",
+    SentimentAnalysisWebsite : "https://devanshsahni.github.io/Rog-Free/",
+
+    NewsletterDesc:"A newsletter signup site made using Mailchimp API where the signups can be monitored from the MailChimp account. This project was made to understand API integration, environment variables and vercel deployment.",
+    NewsletterGithub:"",
+    NewsletterWebsite:"https://newsletter-signup-teal.vercel.app/",
+    
+    WigglesDesc:"The system allows easy access and updating of employee data. It generates timely reports to help monitor employees and support tasks like performance appraisals and promotions.",
+    WigglesGithub:"https://github.com/DevanshSahni/Wiggles",
+    WigglesWebsite:"https://wiggles.vercel.app/",
+  }
+
+  let show ='';
+  if(desc[projectName + 'Github']===""){
+    show="none";
+  }
+    
+  return (
+    <div className='projectBox'> 
+        <img className='projectPhoto' src={projectPhoto} alt="Project display" /> 
+        <div>
+            <br />
+            <h3>{projectName}</h3>
+            <br />
+            {desc[projectName + 'Desc']}
+            <br />
+
+            <a style={{display:show}} href={desc[projectName + 'Github']} target='_blank'>
+              <button className='projectbtn'><FaGithub/> Github</button>
+            </a>
+
+            {/* <a href={desc[projectName + 'Website']} target='_blank'>
+              <button className='projectbtn'><CgFileDocument/> Demo</button>
+            </a> */}
+        </div>
+    </div>
+  )
+}
+
+export default  ProjectBox;
